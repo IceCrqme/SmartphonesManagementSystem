04777d9be00563e50cc326ef733143de7d456201
IceCrqme <123979920+IceCrqme@users.noreply.github.com>
2026-01-21 06:25:57 +0200

    Initial commit with project files and database backup

---
+++ SmartphonesManagementSystem/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace SmartphonesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewSmartphones = new System.Windows.Forms.DataGridView();
+            this.textBoxBrand = new System.Windows.Forms.TextBox();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxRAM = new System.Windows.Forms.TextBox();
+            this.textBoxStorage = new System.Windows.Forms.TextBox();
+            this.labelBrand = new System.Windows.Forms.Label();
+            this.labelModel = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelRAM = new System.Windows.Forms.Label();
+            this.labelStorage = new System.Windows.Forms.Label();
+            this.comboBoxOS = new System.Windows.Forms.ComboBox();
+            this.labelOS = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSmartphones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewSmartphones
+            // 
+            this.dataGridViewSmartphones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSmartphones.Location = new System.Drawing.Point(12, 263);
+            this.dataGridViewSmartphones.Name = "dataGridViewSmartphones";
+            this.dataGridViewSmartphones.Size = new System.Drawing.Size(773, 175);
+            this.dataGridViewSmartphones.TabIndex = 0;
+            this.dataGridViewSmartphones.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewSmartphones_CellClick);
+            // 
+            // textBoxBrand
+            // 
+            this.textBoxBrand.Location = new System.Drawing.Point(62, 12);
+            this.textBoxBrand.Name = "textBoxBrand";
+            this.textBoxBrand.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBrand.TabIndex = 1;
+            // 
+            // textBoxModel
+            // 
+            this.textBoxModel.Location = new System.Drawing.Point(62, 48);
+            this.textBoxModel.Name = "textBoxModel";
+            this.textBoxModel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxModel.TabIndex = 2;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(62, 86);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 3;
+            // 
+            // textBoxRAM
+            // 
+            this.textBoxRAM.Location = new System.Drawing.Point(62, 124);
+            this.textBoxRAM.Name = "textBoxRAM";
+            this.textBoxRAM.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRAM.TabIndex = 4;
+            // 
+            // textBoxStorage
+            // 
+            this.textBoxStorage.Location = new System.Drawing.Point(62, 162);
+            this.textBoxStorage.Name = "textBoxStorage";
+            this.textBoxStorage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStorage.TabIndex = 5;
+            // 
+            // labelBrand
+            // 
+            this.labelBrand.AutoSize = true;
+            this.labelBrand.Location = new System.Drawing.Point(12, 15);
+            this.labelBrand.Name = "labelBrand";
+            this.labelBrand.Size = new System.Drawing.Size(38, 13);
+            this.labelBrand.TabIndex = 6;
+            this.labelBrand.Text = "Brand:";
+            // 
+            // labelModel
+            // 
+            this.labelModel.AutoSize = true;
+            this.labelModel.Location = new System.Drawing.Point(12, 51);
+            this.labelModel.Name = "labelModel";
+            this.labelModel.Size = new System.Drawing.Size(39, 13);
+            this.labelModel.TabIndex = 7;
+            this.labelModel.Text = "Model:";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(12, 89);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(34, 13);
+            this.labelPrice.TabIndex = 8;
+            this.labelPrice.Text = "Price:";
+            // 
+            // labelRAM
+            // 
+            this.labelRAM.AutoSize = true;
+            this.labelRAM.Location = new System.Drawing.Point(12, 127);
+            this.labelRAM.Name = "labelRAM";
+            this.labelRAM.Size = new System.Drawing.Size(34, 13);
+            this.labelRAM.TabIndex = 9;
+            this.labelRAM.Text = "RAM:";
+            // 
+            // labelStorage
+            // 
+            this.labelStorage.AutoSize = true;
+            this.labelStorage.Location = new System.Drawing.Point(9, 165);
+            this.labelStorage.Name = "labelStorage";
+            this.labelStorage.Size = new System.Drawing.Size(47, 13);
+            this.labelStorage.TabIndex = 10;
+            this.labelStorage.Text = "Storage:";
+            // 
+            // comboBoxOS
+            // 
+            this.comboBoxOS.FormattingEnabled = true;
+            this.comboBoxOS.Items.AddRange(new object[] {
+            "Android",
+            "iOS"});
+            this.comboBoxOS.Location = new System.Drawing.Point(253, 11);
+            this.comboBoxOS.Name = "comboBoxOS";
+            this.comboBoxOS.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOS.TabIndex = 11;
+            // 
+            // labelOS
+            // 
+            this.labelOS.AutoSize = true;
+            this.labelOS.Location = new System.Drawing.Point(213, 15);
+            this.labelOS.Name = "labelOS";
+            this.labelOS.Size = new System.Drawing.Size(25, 13);
+            this.labelOS.TabIndex = 12;
+            this.labelOS.Text = "OS:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 198);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(107, 198);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 14;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(203, 198);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 15;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(299, 198);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(12, 227);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 17;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelOS);
+            this.Controls.Add(this.comboBoxOS);
+            this.Controls.Add(this.labelStorage);
+            this.Controls.Add(this.labelRAM);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelModel);
+            this.Controls.Add(this.labelBrand);
+            this.Controls.Add(this.textBoxStorage);
+            this.Controls.Add(this.textBoxRAM);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxModel);
+            this.Controls.Add(this.textBoxBrand);
+            this.Controls.Add(this.dataGridViewSmartphones);
+            this.Name = "Form1";
+            this.Text = "Smartphones Management System";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSmartphones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewSmartphones;
+        private System.Windows.Forms.TextBox textBoxBrand;
+        private System.Windows.Forms.TextBox textBoxModel;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxRAM;
+        private System.Windows.Forms.TextBox textBoxStorage;
+        private System.Windows.Forms.Label labelBrand;
+        private System.Windows.Forms.Label labelModel;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label labelRAM;
+        private System.Windows.Forms.Label labelStorage;
+        private System.Windows.Forms.ComboBox comboBoxOS;
+        private System.Windows.Forms.Label labelOS;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
---
+++ SmartphonesManagementSystem/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartphonesApp
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=SmartphonesDB;Integrated Security=True";
+        int selectedId = -1;
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Smartphones", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridViewSmartphones.DataSource = dt;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void ClearFields()
+        {
+            textBoxBrand.Clear();
+            textBoxModel.Clear();
+            textBoxPrice.Clear();
+            textBoxRAM.Clear();
+            textBoxStorage.Clear();
+            comboBoxOS.SelectedIndex = -1;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Smartphones (Brand, Model, OS, Price, RAM, Storage) " +
+                               "VALUES (@Brand, @Model, @OS, @Price, @RAM, @Storage)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Brand", textBoxBrand.Text);
+                cmd.Parameters.AddWithValue("@Model", textBoxModel.Text);
+                cmd.Parameters.AddWithValue("@OS", comboBoxOS.Text);
+                cmd.Parameters.AddWithValue("@Price", decimal.Parse(textBoxPrice.Text));
+                cmd.Parameters.AddWithValue("@RAM", int.Parse(textBoxRAM.Text));
+                cmd.Parameters.AddWithValue("@Storage", int.Parse(textBoxStorage.Text));
+
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadData();
+            ClearFields();
+        }
+
+        private void dataGridViewSmartphones_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            DataGridViewRow row = dataGridViewSmartphones.Rows[e.RowIndex];
+
+            if (row.Cells["Id"].Value == DBNull.Value)
+                return;
+
+            selectedId = Convert.ToInt32(row.Cells["Id"].Value);
+
+            textBoxBrand.Text = row.Cells["Brand"].Value.ToString();
+            textBoxModel.Text = row.Cells["Model"].Value.ToString();
+            comboBoxOS.Text = row.Cells["OS"].Value.ToString();
+            textBoxPrice.Text = row.Cells["Price"].Value.ToString();
+            textBoxRAM.Text = row.Cells["RAM"].Value.ToString();
+            textBoxStorage.Text = row.Cells["Storage"].Value.ToString();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+            selectedId = -1;
+            LoadData();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedId == -1)
+            {
+                MessageBox.Show("Please select a record to update.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "UPDATE Smartphones SET " +
+                               "Brand = @Brand, " +
+                               "Model = @Model, " +
+                               "OS = @OS, " +
+                               "Price = @Price, " +
+                               "RAM = @RAM, " +
+                               "Storage = @Storage " +
+                               "WHERE Id = @Id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Brand", textBoxBrand.Text);
+                cmd.Parameters.AddWithValue("@Model", textBoxModel.Text);
+                cmd.Parameters.AddWithValue("@OS", comboBoxOS.Text);
+                cmd.Parameters.AddWithValue("@Price", decimal.Parse(textBoxPrice.Text));
+                cmd.Parameters.AddWithValue("@RAM", int.Parse(textBoxRAM.Text));
+                cmd.Parameters.AddWithValue("@Storage", int.Parse(textBoxStorage.Text));
+                cmd.Parameters.AddWithValue("@Id", selectedId);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadData();
+            ClearFields();
+            selectedId = -1;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedId == -1)
+            {
+                MessageBox.Show("Please select a record to delete.");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Are you sure you want to delete this record?", "Confirm Delete", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "DELETE FROM Smartphones WHERE Id = @Id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Id", selectedId);
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadData();
+                ClearFields();
+                selectedId = -1;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Smartphones WHERE 1=1";
+
+                if (!string.IsNullOrWhiteSpace(textBoxBrand.Text))
+                {
+                    query += " AND Brand LIKE @Brand";
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxModel.Text))
+                {
+                    query += " AND Model LIKE @Model";
+                }
+
+                if (!string.IsNullOrWhiteSpace(comboBoxOS.Text))
+                {
+                    query += " AND OS = @OS";
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxPrice.Text))
+                {
+                    query += " AND Price = @Price";
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxRAM.Text))
+                {
+                    query += " AND RAM = @RAM";
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxStorage.Text))
+                {
+                    query += " AND Storage = @Storage";
+                }
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                if (!string.IsNullOrWhiteSpace(textBoxBrand.Text))
+                {
+                    cmd.Parameters.AddWithValue("@Brand", "%" + textBoxBrand.Text + "%");
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxModel.Text))
+                {
+                    cmd.Parameters.AddWithValue("@Model", "%" + textBoxModel.Text + "%");
+                }
+
+                if (!string.IsNullOrWhiteSpace(comboBoxOS.Text))
+                {
+                    cmd.Parameters.AddWithValue("@OS", comboBoxOS.Text);
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxPrice.Text))
+                {
+                    cmd.Parameters.AddWithValue("@Price", decimal.Parse(textBoxPrice.Text));
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxRAM.Text))
+                {
+                    cmd.Parameters.AddWithValue("@RAM", int.Parse(textBoxRAM.Text));
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBoxStorage.Text))
+                {
+                    cmd.Parameters.AddWithValue("@Storage", int.Parse(textBoxStorage.Text));
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridViewSmartphones.DataSource = dt;
+            }
+        }
+    }
+}
